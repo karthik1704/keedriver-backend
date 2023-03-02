d2bf9d39ebf68c861dfccaf4b5b3d675751545b0
Karthi A <karthikthee7@gmail.com>
2023-03-02 03:37:55 +0000

    production change

--- keedriver/settings.py
+++ keedriver/settings.py
@@ -142,8 +142,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_ROOT = BASE_DIR / "static"
-STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static/"
+STATIC_URL = "/static/"
 
 # Base url to serve media files
 MEDIA_URL = "/media/"
