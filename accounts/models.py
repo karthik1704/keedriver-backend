7c57355e25422b75a86b4a2d42c1154e42c7cbfb
Karthi A <karthikthee7@gmail.com>
2023-09-19 10:15:32 +0530

    remove / for acccounts

--- accounts/models.py
+++ accounts/models.py
@@ -110,6 +110,6 @@ class DriverProfile(models.Model):
     license_expiry_date = models.DateField()
     aadhaar_number = models.CharField(max_length=20, null=True, blank=True)
     is_avaliable = models.BooleanField(default=False)
-\
+
     def __str__(self):
         return self.driver.get_full_name()
