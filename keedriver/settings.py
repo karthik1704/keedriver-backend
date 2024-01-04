305cb5ecb41bd8055be89b4dcab197caadfa4267
Karthi A <karthikthee7@gmail.com>
2024-01-04 21:15:12 +0530

    prod settings

--- keedriver/settings.py
+++ keedriver/settings.py
@@ -118,22 +118,22 @@ if DEBUG:
         #     "ENGINE": "django.db.backends.sqlite3",
         #     "NAME": BASE_DIR / "db.sqlite3",
         # }
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": "keedriver_dev",
-            "USER": env("DB_USER"),
-            "PASSWORD": env("DB_PASSWORD"),
-            "HOST": env("DB_HOST"),
-            "PORT": env("DB_PORT"),
-        }
         # "default": {
         #     "ENGINE": "django.db.backends.postgresql",
         #     "NAME": "keedriver_dev",
-        #     "USER": "postgres",
-        #     "PASSWORD": "postgres",
-        #     "HOST": "localhost",
+        #     "USER": env("DB_USER"),
+        #     "PASSWORD": env("DB_PASSWORD"),
+        #     "HOST": env("DB_HOST"),
         #     "PORT": env("DB_PORT"),
         # }
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "keedriver_dev",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "localhost",
+            "PORT": env("DB_PORT"),
+        }
     }
 
 if not DEBUG:
