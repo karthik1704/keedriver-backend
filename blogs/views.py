930c8b4b376794e7b1cc88e4f7644a74ce9a2c41
Karthi A <karthikthee7@gmail.com>
2024-05-01 10:26:11 +0530

    some changes

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.permissions import AllowAny
 
 from blogs.models import Blog
 from blogs.serializers import BlogSerializer
@@ -10,12 +11,14 @@ from blogs.serializers import BlogSerializer
 class BlogList(ListAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
+    permission_classes = [AllowAny]
 
 
 class BlogDetail(RetrieveAPIView):
     queryset = Blog.objects.none
     serializer_class = BlogSerializer
     lookup_field = "slug"
+    permission_classes = [AllowAny]
 
     def get_queryset(self):
         return Blog.objects.all()
