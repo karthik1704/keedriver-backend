93ce8ba3742823748208b81b2162bbad39c054a9
Mohammed Zaman <zamanmohammed5152@gmail.com>
2024-05-10 11:25:27 +0530

    fix url

--- contacts/urls.py
+++ contacts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ContactCreateView
 
 urlpatterns = [
-    path('contacts/add/',ContactCreateView.as_view()),
+    path('contacts/',ContactCreateView.as_view()),
 ]
