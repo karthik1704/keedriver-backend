2e013ef74b5419a97f92c1582852a12e149c1622
Mohammed Zaman <zamanmohammed5152@gmail.com>
2024-05-10 11:49:18 +0530

    fix models

--- contacts/models.py
+++ contacts/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class contact(models.Model):
 
     name=models.CharField(max_length=200,null=True)
-    Email=models.EmailField(max_length=30)
-    Phone=models.CharField(max_length=50)
-    Message=models.TextField(blank=True)
+    email=models.EmailField(max_length=30)
+    phone=models.CharField(max_length=50)
+    message=models.TextField(blank=True)
 
     def __str__(self):
         return self.name
