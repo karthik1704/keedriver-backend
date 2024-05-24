bd68c5ab2f723d8ee69ed2c994ca867b009bc2f7
Mohammed Zaman <zamanmohammed5152@gmail.com>
2024-05-24 18:56:54 +0530

    get api for reviewer and trip

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ReviewByTripId,ReviewCreateTripId
 
 urlpatterns = [
-    path('review/<int:pk>',ReviewByTripId.as_view()),
+    path('review/<int:trip_id>/',ReviewByTripId.as_view()),
     path('review/create/',ReviewCreateTripId.as_view()),
 
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,17 +1,25 @@
+from tkinter import NO
 from rest_framework import generics
 
 from keedriver import permissions
+from trips.models import Trip
 
 from .models import Review
 from .serializers import ReviewCreateSerializer, ReviewSerialzer
 
 
-class ReviewByTripId(generics.RetrieveAPIView):
-    queryset = Review.objects.all()
+class ReviewByTripId(generics.ListAPIView):
+    
     serializer_class=ReviewSerialzer
     permission_classes = {permissions.BasePermission}
+    
 
+    def get_queryset(self):
+        trip_id = self.kwargs['trip_id']
+        return Review.objects.filter(reviewer=self.request.user, trip=trip_id)
+        
 
+        
 
 class ReviewCreateTripId(generics.CreateAPIView):
     queryset = Review.objects.all()
